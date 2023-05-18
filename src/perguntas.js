61d93a8db19ee01a39b35ec42d9825954f27b90b
draxter-07 <philippe.idalgoprestes@gmail.com>
2023-05-18 11:45:08 -0300

    Layout e componentização: criação dos botões e scripts iniciada e quase terminada

--- src/perguntas.js
+++ src/perguntas.js
@@ -1,45 +1,115 @@
 import styled from "styled-components"
 import React from 'react'
+import seta_play from './assets/seta_play.png'
 
 export default function Perguntas(){
-    let perguntas = [1, 2, 3, 4, 5, 6, 7, 8];
-    const [cor, setCor] = React.useState('rgb(250, 250, 50)');
-    function clic(e){
-        e.target.style.display = 'none';
-        if (cor == 'rgb(250, 250, 50)'){
-            setCor('rgb(255, 255, 255)');
+    let perguntas = [['Pergunta1', 'Resposta1'], ['Pergunta2', 'Resposta2']];
+    function show_question(e){
+        let clicked = e.target.nodeName;
+        let element;
+        if (clicked == 'IMG'){
+            element = e.target.parentElement.parentElement.parentElement.parentElement;
         }
         else{
-            setCor('rgb(250, 250, 50)');
+            element = e.target.parentElement.parentElement.parentElement;
         }
+        element.children[0].style.display = 'none';
+        element.children[1].style.display = 'flex';
     }
-    const Pergunta = styled.button`
+    function show_answer(e){
+        let element = e.target.parentElement.parentElement.parentElement;
+        element.children[1].style.display = 'none';
+        element.children[2].style.display = 'flex';
+    }
+    const Parte = styled.div`
+    @media (min-width: 1px){}`;
+    const Question = styled.span`
+    @media (min-width: 1px){
+        font-size: 40px;
+        text-align: left;
+        width: 100%;
+    }`;
+    const Virar = styled.button`
+    @media (min-width: 1px){
+        background: none;
+        border: none;
+    }`;
+    const Botoes = styled.div`
+    @media (min-width: 1px){
+        background: none;
+        border: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        align-items: center;
+        width: 100%;
+    }`;
+    const Pergunta = styled.div`
     @media (min-width: 1px){
         box-sizing: border-box;
         width: 100%;
-        background-color: ${cor};
+        background-color: rgb(250, 250, 50);
+        padding: 20px 30px;
+        border-radius: 15px;
+        display: none;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0px 0px 20px;
+    }`;
+    const Holder = styled.div`
+    @media (min-width: 1px){
+        box-sizing: border-box;
+        width: 100%;
+        background-color: rgb(250, 250, 50);
         padding: 20px 30px;
         border-radius: 15px;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
+        align-items: center;
         margin: 0px 0px 20px;
-        border: none;
     }`;
-    const Question = styled.span`
+    const Resposta = styled.div`
     @media (min-width: 1px){
-        font-size: 40px;
-    }`;
-    const Virar = styled.img`
-    @media (min-width: 1px){;
+        box-sizing: border-box;
+        width: 100%;
+        background-color: rgb(250, 250, 50);
+        padding: 20px 30px;
+        border-radius: 15px;
+        display: none;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0px 0px 20px;
     }`;
     return(
         <>
         {perguntas.map((pergunta) =>
-        <Pergunta onClick={(e) => clic(e)}>
-            <Question>Pergunta {pergunta}</Question>
-            <Virar src="./assets/seta_play.png"></Virar>
-        </Pergunta>
+        <Parte id={perguntas.indexOf(pergunta)}>
+            <Holder>
+                <Question>Pergunta {perguntas.indexOf(pergunta) + 1}</Question>
+                <Botoes>
+                    <Virar onClick={(e) => show_question(e)}>
+                        <img src={seta_play}></img>
+                    </Virar>
+                </Botoes>
+            </Holder>
+            <Pergunta>
+                <Question>{pergunta[0]}</Question>
+                <Botoes>
+                    <button onClick={(e) => show_answer(e)}>Oie</button>
+                </Botoes>
+            </Pergunta>
+            <Resposta>
+                <Question>{pergunta[1]}</Question>
+                <Botoes>
+                    <button>Oie</button>
+                    <button>Oie</button>
+                    <button>Oie</button>
+                </Botoes>
+            </Resposta>
+        </Parte>
         )}
         </>
     )
